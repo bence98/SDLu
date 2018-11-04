a8cfac2a4f9bf4ff7c9c1a7b5aa503b70f1725ba
Bence Csókás <bence.csokas@gmail.com>
2018-11-04 19:40:34 +0100

    Calling `SDL_Init()` from `main()`

--- src/sdlu.c
+++ src/sdlu.c
@@ -1,20 +1,14 @@
 #include "sdlu.h"
 #include <string.h>
+#include <stdio.h>
 
-/* SDLu v1.1.-1
+/* SDLu v1.1.2-pre1
  * (c) 2018, Csókás Bence Viktor
  */
 
-bool _sdlu_init=false, _sdlu_dbg=false;
+bool _sdlu_dbg=false;
 
 SDL_Renderer* sdlu_create_win_renderer(char* title, unsigned int w, unsigned int h){
-	if(!_sdlu_init){
-		if(SDL_Init(SDL_INIT_EVERYTHING)<0)
-			return NULL;
-		else
-			_sdlu_init=true;
-	}
-	
 	SDL_Window* wnd=SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, w, h, 0);
 	if(wnd==NULL) return NULL;
 	return SDL_CreateRenderer(wnd, -1, SDL_RENDERER_SOFTWARE);
@@ -28,6 +22,11 @@ int main(int argc, char* argv[]){
 	for(int i=1;i<argc;i++)
 		if(strcmp("--debug", argv[i])==0)
 			_sdlu_dbg=true;
+	
+	if(SDL_Init(SDL_INIT_EVERYTHING)<0){
+		fprintf(stderr, "SDL error!\n");
+		return 1;
+	}
 	
 	sdlu_OnInit();
 	SDL_Event evt;
